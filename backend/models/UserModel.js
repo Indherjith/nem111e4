d1de297a5be8ebcd962ca0e841ccccf8c4940998
Indherjith <buvanesifet26@gmail.com>
2022-11-07 17:37:34 +0530

    main

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
     email:{type:String, requires:true},
-    password:{type:String, requires:true},
-    mobile:{type:String, requires:true},
+    password:{type:String, requires:true}
 })
 const UserModel = mongoose.model("user",userSchema)
 module.exports = {UserModel}
